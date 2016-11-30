6b944e5d91b8873258ac52b3a2ae75d54176d242
dwhanger <dwhanger@hotmail.com>
2016-11-29 19:53:32 -0800

    edits to verify_service.bat

--- _scripts/verify_service.bat
+++ _scripts/verify_service.bat
@@ -1,12 +1,3 @@
-#!/bin/bash
-
-# result=$(curl http://localhost/index.html/)
-# if [[ "$result" =~ "Hello World" ]]; then
-#     exit 0
-# else
-#     exit 1
-# fi
-
 $result = (curl http://localhost/index.html).StatusCode
 
 if ("$result" -eq "200") { 
