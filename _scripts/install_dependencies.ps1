28b57beb6b49a7f330917acf1e2a2d5bbe8eb17b
dwhanger <dwhanger@hotmail.com>
2016-11-30 09:46:08 -0800

    cleanup of .ps1 script

--- _scripts/install_dependencies.ps1
+++ _scripts/install_dependencies.ps1
@@ -1,7 +1,7 @@
-# REM Called from a .bat file for the below line...
-# REM PowerShell.exe -Command "& {Start-Process PowerShell.exe -ArgumentList '-ExecutionPolicy Bypass -File ""%~dpn0.ps1""' -Verb RunAs}"
-# REM 
-# powershell.exe -Command Import-Module -Name ServerManager
-# powershell.exe -Command Install-WindowsFeature Web-Server -IncludeAllSubFeature -IncludeManagementTools
+# Wow....this works!  Setup a .bat file to be called from your appspec.yml and then have the .bat file call powershell with this same base named powershell script file...
+#
+# Example of doing this with the %~dpn0 moniker in order to get to the base filename...
+#     PowerShell.exe -Command "& {Start-Process PowerShell.exe -ArgumentList '-ExecutionPolicy Bypass -File ""%~dpn0.ps1""' -Verb RunAs}"
+# 
 PowerShell.exe -Command "& {Start-Process PowerShell.exe -ArgumentList 'Import-Module -Name ServerManager' -Verb RunAs}"
 PowerShell.exe -Command "& {Start-Process PowerShell.exe -ArgumentList 'Install-WindowsFeature Web-Server -IncludeAllSubFeature -IncludeManagementTools' -Verb RunAs}"
