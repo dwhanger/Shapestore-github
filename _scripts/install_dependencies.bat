8d3ca8bd033afc20913f6b6e8fb6649e993a23a8
dwhanger <dwhanger@hotmail.com>
2016-11-29 19:39:42 -0800

    REM ing out parts of file

--- _scripts/install_dependencies.bat
+++ _scripts/install_dependencies.bat
@@ -1,3 +1,2 @@
-powershell.exe -Command Import-Module -Name ServerManager
-$P = powershell.exe -Command Install-WindowsFeature Web-Server -IncludeAllSubFeature -IncludeManagementTools
-if ($P -match "True") { exit 0 } else { exit 1 }
+REM powershell.exe -Command Import-Module -Name ServerManager
+REM powershell.exe -Command Install-WindowsFeature Web-Server -IncludeAllSubFeature -IncludeManagementTools
