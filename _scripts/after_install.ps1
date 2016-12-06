0b16f09971a330fd52f8149d7d6b1eb15d204799
dwhanger <dwhanger@hotmail.com>
2016-12-06 14:11:59 -0800

    webconfig and after install changes

--- _scripts/after_install.ps1
+++ _scripts/after_install.ps1
@@ -1,18 +1,5 @@
 # Fixup the strings in the web.config and other places after the deployment....
 #  
-# If ( "$DEPLOYMENT_GROUP_NAME" == "TEST%" )
-# {
-#	(Get-Content c:\inetpub\wwwroot\shapestore\Web.config) | 
-#	Foreach-Object {$_ -replace '%%DYNAMODB_TABLE_STRING%%','TEST-ASP.NET_SessionState'}  | 
-#	Out-File c:\inetpub\wwwroot\shapestore\Web.config
-# 
-# }
-# ElseIf ( "$DEPLOYMENT_GROUP_NAME" == "DEV%" )
-# {
-#	(Get-Content c:\inetpub\wwwroot\shapestore\Web.config) | 
-#	Foreach-Object {$_ -replace '%%DYNAMODB_TABLE_STRING%%','DEV-ASP.NET_SessionState'}  | 
-#	Out-File c:\inetpub\wwwroot\shapestore\Web.config
-# }
 
 If ( $env:DEPLOYMENT_GROUP_NAME -like "TEST*" )
 {
