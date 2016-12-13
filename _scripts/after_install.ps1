b8157196f1e08a42477428b7cd9c945359ffcb3b
dwhanger <dwhanger@hotmail.com>
2016-12-13 10:30:18 -0800

    using BatchSubstitute to do the string find and replace

--- _scripts/after_install.ps1
+++ _scripts/after_install.ps1
@@ -4,69 +4,33 @@
 
 If ( $env:DEPLOYMENT_GROUP_NAME -like "TEST*" )
 {
-	(Get-Content c:\inetpub\wwwroot\shapestore\Web.config) | 
-	Foreach-Object {$_ -replace '%%MYSQLDB_STRING%%','testdmw2shapestoreinstance.cxkjrsk8xjz1.us-west-2.rds.amazonaws.com'}  | 
-	Out-File c:\inetpub\wwwroot\shapestore\Web.config 
+	call BatchSubstitute.bat ___MYSQLDB_STRING___ testdmw2shapestoreinstance.cxkjrsk8xjz1.us-west-2.rds.amazonaws.com c:\inetpub\wwwroot\shapestore\Web.config > c:\temp\Web1.text
+	call BatchSubstitute.bat ___DYNAMODB_TABLE_STRING___ TEST-ASP.NET_SessionState c:\temp\Web1.text > c:\temp\Web2.text
+	call BatchSubstitute.bat ___SQS_QUEUE_NAME_STRING___ https://sqs.us-west-2.amazonaws.com/591516837963/TEST-dmw2-shapeorders-queue c:\temp\Web2.text > c:\temp\Web3.text
+	call BatchSubstitute.bat ___S3_BUCKET_NAME_STRING___ test-dmw2-shapestoreresources c:\temp\Web3.text > c:\temp\Web4.text
+	call BatchSubstitute.bat ___MYSQLDB_DBNAME_STRING___ TESTdmw2shapestoredb c:\temp\Web4.text > c:\temp\Web5.text
+	call BatchSubstitute.bat ___MYSQLDB_USER_STRING___ admin c:\temp\Web5.text > c:\temp\Web6.text
+	call BatchSubstitute.bat ___MYSQLDB_PASSWORD_STRING___ Password1234 c:\temp\Web6.text > c:\temp\Web7.text
+	mv c:\temp\web7.text c:\inetpub\wwwroot\shapestore\Web.config
+	del c:\temp\web?.text
 
-	(Get-Content c:\inetpub\wwwroot\shapestore\Web.config) | 
-	Foreach-Object {$_ -replace '%%DYNAMODB_TABLE_STRING%%','TEST-ASP.NET_SessionState'}  | 
-	Out-File c:\inetpub\wwwroot\shapestore\Web.config
-
-	(Get-Content c:\inetpub\wwwroot\shapestore\Web.config) | 
-	Foreach-Object {$_ -replace '%%SQS_QUEUE_NAME_STRING%%','https://sqs.us-west-2.amazonaws.com/591516837963/TEST-dmw2-shapeorders-queue'}  | 
-	Out-File c:\inetpub\wwwroot\shapestore\Web.config
-
-	(Get-Content c:\inetpub\wwwroot\shapestore\Web.config) | 
-	Foreach-Object {$_ -replace '%%S3_BUCKET_NAME_STRING%%','test-dmw2-shapestoreresources'}  | 
-	Out-File c:\inetpub\wwwroot\shapestore\Web.config
-
-	(Get-Content c:\inetpub\wwwroot\shapestore\Web.config) | 
-	Foreach-Object {$_ -replace '%%MYSQLDB_DBNAME_STRING%%','TESTdmw2shapestoredb'}  | 
-	Out-File c:\inetpub\wwwroot\shapestore\Web.config
-
-	(Get-Content c:\inetpub\wwwroot\shapestore\Web.config) | 
-	Foreach-Object {$_ -replace '%%MYSQLDB_USER_STRING%%','admin'}  | 
-	Out-File c:\inetpub\wwwroot\shapestore\Web.config
-
-	(Get-Content c:\inetpub\wwwroot\shapestore\Web.config) | 
-	Foreach-Object {$_ -replace '%%MYSQLDB_PASSWORD_STRING%%','Password1234'}  | 
-	Out-File c:\inetpub\wwwroot\shapestore\Web.config
+#
+# Unfortunately, the following does not work because it inserts a LF in the output text along with the CR and there is a way to suppress that with the OutFile cmdlet with a -NoNewLine but...
+# that is only available in Powershell 5.....
+#
+#	(Get-Content c:\inetpub\wwwroot\shapestore\Web.config) | 
+#	Foreach-Object {$_ -replace '%%MYSQLDB_STRING%%','testdmw2shapestoreinstance.cxkjrsk8xjz1.us-west-2.rds.amazonaws.com'}  | 
+#	Out-File c:\inetpub\wwwroot\shapestore\Web.config 
 }
 ElseIf ( $env:DEPLOYMENT_GROUP_NAME -like "DEV*" )
 {
-#	(Get-Content c:\inetpub\wwwroot\shapestore\Web.config) | 
-#	Foreach-Object {$_ -replace '%%MYSQLDB_STRING%%','dmw2shapestoreinstance.cxkjrsk8xjz1.us-west-2.rds.amazonaws.com'}  | 
-#	Out-File c:\inetpub\wwwroot\shapestore\Web.config -NoNewLine
-	"C:\temp\fnr.exe" --cl --dir "c:\inetpub\wwwroot\shapestore" --fileMask "Web.config" --find "%%MYSQLDB_STRING%%" --replace "dmw2shapestoreinstance.cxkjrsk8xjz1.us-west-2.rds.amazonaws.com"
-
-#	(Get-Content c:\inetpub\wwwroot\shapestore\Web.config) | 
-#	Foreach-Object {$_ -replace '%%DYNAMODB_TABLE_STRING%%','DEV-ASP.NET_SessionState'}  | 
-#	Out-File c:\inetpub\wwwroot\shapestore\Web.config
-	"C:\temp\fnr.exe" --cl --dir "c:\inetpub\wwwroot\shapestore" --fileMask "Web.config" --find "%%DYNAMODB_TABLE_STRING%%" --replace "DEV-ASP.NET_SessionState"
-
-
-#	(Get-Content c:\inetpub\wwwroot\shapestore\Web.config) | 
-#	Foreach-Object {$_ -replace '%%SQS_QUEUE_NAME_STRING%%','https://sqs.us-west-2.amazonaws.com/591516837963/DEV-dmw2-shapeorders-queue'}  | 
-#	Out-File c:\inetpub\wwwroot\shapestore\Web.config
-	"C:\temp\fnr.exe" --cl --dir "c:\inetpub\wwwroot\shapestore" --fileMask "Web.config" --find "%%SQS_QUEUE_NAME_STRING%%" --replace "https://sqs.us-west-2.amazonaws.com/591516837963/DEV-dmw2-shapeorders-queue"
-#
-#	(Get-Content c:\inetpub\wwwroot\shapestore\Web.config) | 
-#	Foreach-Object {$_ -replace '%%S3_BUCKET_NAME_STRING%%','dev-dmw2-shapestoreresources'}  | 
-#	Out-File c:\inetpub\wwwroot\shapestore\Web.config
-	"C:\temp\fnr.exe" --cl --dir "c:\inetpub\wwwroot\shapestore" --fileMask "Web.config" --find "%%S3_BUCKET_NAME_STRING%%" --replace "dev-dmw2-shapestoreresources"
-#
-#	(Get-Content c:\inetpub\wwwroot\shapestore\Web.config) | 
-#	Foreach-Object {$_ -replace '%%MYSQLDB_DBNAME_STRING%%','DEVdmw2shapestoredb'}  | 
-#	Out-File c:\inetpub\wwwroot\shapestore\Web.config
-	"C:\temp\fnr.exe" --cl --dir "c:\inetpub\wwwroot\shapestore" --fileMask "Web.config" --find "%%MYSQLDB_DBNAME_STRING%%" --replace "dmw2shapestoredb"
-#
-#	(Get-Content c:\inetpub\wwwroot\shapestore\Web.config) | 
-#	Foreach-Object {$_ -replace '%%MYSQLDB_USER_STRING%%','admin'}  | 
-#	Out-File c:\inetpub\wwwroot\shapestore\Web.config
-	"C:\temp\fnr.exe" --cl --dir "c:\inetpub\wwwroot\shapestore" --fileMask "Web.config" --find "%%MYSQLDB_USER_STRING%%" --replace "admin"
-#
-#	(Get-Content c:\inetpub\wwwroot\shapestore\Web.config) | 
-#	Foreach-Object {$_ -replace '%%MYSQLDB_PASSWORD_STRING%%','Password1234'}  | 
-#	Out-File c:\inetpub\wwwroot\shapestore\Web.config
-	"C:\temp\fnr.exe" --cl --dir "c:\inetpub\wwwroot\shapestore" --fileMask "Web.config" --find "%%MYSQLDB_PASSWORD_STRING%%" --replace "j57Q25oTosk2sWybLr3f;"
+	call BatchSubstitute.bat ___MYSQLDB_STRING___ dmw2shapestoreinstance.cxkjrsk8xjz1.us-west-2.rds.amazonaws.com c:\inetpub\wwwroot\shapestore\Web.config > c:\temp\Web1.text
+	call BatchSubstitute.bat ___DYNAMODB_TABLE_STRING___ DEV-ASP.NET_SessionState c:\temp\Web1.text > c:\temp\Web2.text
+	call BatchSubstitute.bat ___SQS_QUEUE_NAME_STRING___ https://sqs.us-west-2.amazonaws.com/591516837963/DEV-dmw2-shapeorders-queue c:\temp\Web2.text > c:\temp\Web3.text
+	call BatchSubstitute.bat ___S3_BUCKET_NAME_STRING___ dev-dmw2-shapestoreresources c:\temp\Web3.text > c:\temp\Web4.text
+	call BatchSubstitute.bat ___MYSQLDB_DBNAME_STRING___ dmw2shapestoredb c:\temp\Web4.text > c:\temp\Web5.text
+	call BatchSubstitute.bat ___MYSQLDB_USER_STRING___ admin c:\temp\Web5.text > c:\temp\Web6.text
+	call BatchSubstitute.bat ___MYSQLDB_PASSWORD_STRING___ j57Q25oTosk2sWybLr3f; c:\temp\Web6.text > c:\temp\Web7.text
+	mv c:\temp\web7.text c:\inetpub\wwwroot\shapestore\Web.config
+	del c:\temp\web?.text
 }
