85f283ab4b36675360870dfd80f38b5595132090
dwhanger <dwhanger@hotmail.com>
2016-12-13 11:10:38 -0800

    updated .bat and .ps1 scripts for after_install

--- _scripts/after_install.bat
+++ _scripts/after_install.bat
@@ -1,2 +1,31 @@
-@ECHO OFF
+REM @ECHO OFF
 PowerShell.exe -ExecutionPolicy Bypass -Command "& '%~dpn0.ps1'"
+
+REM Fixup the strings in the web.config and other places after the deployment....
+REM 
+REM 
+
+If ( $DEPLOYMENT_GROUP_NAME == "TEST*" )
+{
+	call BatchSubstitute.bat ___MYSQLDB_STRING___ testdmw2shapestoreinstance.cxkjrsk8xjz1.us-west-2.rds.amazonaws.com c:\inetpub\wwwroot\shapestore\Web.config > c:\temp\Web1.text
+	call BatchSubstitute.bat ___DYNAMODB_TABLE_STRING___ TEST-ASP.NET_SessionState c:\temp\Web1.text > c:\temp\Web2.text
+	call BatchSubstitute.bat ___SQS_QUEUE_NAME_STRING___ https://sqs.us-west-2.amazonaws.com/591516837963/TEST-dmw2-shapeorders-queue c:\temp\Web2.text > c:\temp\Web3.text
+	call BatchSubstitute.bat ___S3_BUCKET_NAME_STRING___ test-dmw2-shapestoreresources c:\temp\Web3.text > c:\temp\Web4.text
+	call BatchSubstitute.bat ___MYSQLDB_DBNAME_STRING___ TESTdmw2shapestoredb c:\temp\Web4.text > c:\temp\Web5.text
+	call BatchSubstitute.bat ___MYSQLDB_USER_STRING___ admin c:\temp\Web5.text > c:\temp\Web6.text
+	call BatchSubstitute.bat ___MYSQLDB_PASSWORD_STRING___ Password1234 c:\temp\Web6.text > c:\temp\Web7.text
+	mv c:\temp\web7.text c:\inetpub\wwwroot\shapestore\Web.config
+REM del c:\temp\web?.text
+}
+ElseIf ( $DEPLOYMENT_GROUP_NAME == "DEV*" )
+{
+	call BatchSubstitute.bat ___MYSQLDB_STRING___ dmw2shapestoreinstance.cxkjrsk8xjz1.us-west-2.rds.amazonaws.com c:\inetpub\wwwroot\shapestore\Web.config > c:\temp\Web1.text
+	call BatchSubstitute.bat ___DYNAMODB_TABLE_STRING___ DEV-ASP.NET_SessionState c:\temp\Web1.text > c:\temp\Web2.text
+	call BatchSubstitute.bat ___SQS_QUEUE_NAME_STRING___ https://sqs.us-west-2.amazonaws.com/591516837963/DEV-dmw2-shapeorders-queue c:\temp\Web2.text > c:\temp\Web3.text
+	call BatchSubstitute.bat ___S3_BUCKET_NAME_STRING___ dev-dmw2-shapestoreresources c:\temp\Web3.text > c:\temp\Web4.text
+	call BatchSubstitute.bat ___MYSQLDB_DBNAME_STRING___ dmw2shapestoredb c:\temp\Web4.text > c:\temp\Web5.text
+	call BatchSubstitute.bat ___MYSQLDB_USER_STRING___ admin c:\temp\Web5.text > c:\temp\Web6.text
+	call BatchSubstitute.bat ___MYSQLDB_PASSWORD_STRING___ j57Q25oTosk2sWybLr3f; c:\temp\Web6.text > c:\temp\Web7.text
+	mv c:\temp\web7.text c:\inetpub\wwwroot\shapestore\Web.config
+REM	del c:\temp\web?.text
+}
--- _scripts/after_install.ps1
+++ _scripts/after_install.ps1
@@ -1,19 +1,9 @@
-# Fixup the strings in the web.config and other places after the deployment....
+# 
 #  
 #
 
 If ( $env:DEPLOYMENT_GROUP_NAME -like "TEST*" )
 {
-	call BatchSubstitute.bat ___MYSQLDB_STRING___ testdmw2shapestoreinstance.cxkjrsk8xjz1.us-west-2.rds.amazonaws.com c:\inetpub\wwwroot\shapestore\Web.config > c:\temp\Web1.text
-	call BatchSubstitute.bat ___DYNAMODB_TABLE_STRING___ TEST-ASP.NET_SessionState c:\temp\Web1.text > c:\temp\Web2.text
-	call BatchSubstitute.bat ___SQS_QUEUE_NAME_STRING___ https://sqs.us-west-2.amazonaws.com/591516837963/TEST-dmw2-shapeorders-queue c:\temp\Web2.text > c:\temp\Web3.text
-	call BatchSubstitute.bat ___S3_BUCKET_NAME_STRING___ test-dmw2-shapestoreresources c:\temp\Web3.text > c:\temp\Web4.text
-	call BatchSubstitute.bat ___MYSQLDB_DBNAME_STRING___ TESTdmw2shapestoredb c:\temp\Web4.text > c:\temp\Web5.text
-	call BatchSubstitute.bat ___MYSQLDB_USER_STRING___ admin c:\temp\Web5.text > c:\temp\Web6.text
-	call BatchSubstitute.bat ___MYSQLDB_PASSWORD_STRING___ Password1234 c:\temp\Web6.text > c:\temp\Web7.text
-	mv c:\temp\web7.text c:\inetpub\wwwroot\shapestore\Web.config
-#	del c:\temp\web?.text
-
 #
 # Unfortunately, the following does not work because it inserts a LF in the output text along with the CR and there is a way to suppress that with the OutFile cmdlet with a -NoNewLine but...
 # that is only available in Powershell 5.....
@@ -24,13 +14,7 @@ If ( $env:DEPLOYMENT_GROUP_NAME -like "TEST*" )
 }
 ElseIf ( $env:DEPLOYMENT_GROUP_NAME -like "DEV*" )
 {
-	call BatchSubstitute.bat ___MYSQLDB_STRING___ dmw2shapestoreinstance.cxkjrsk8xjz1.us-west-2.rds.amazonaws.com c:\inetpub\wwwroot\shapestore\Web.config > c:\temp\Web1.text
-	call BatchSubstitute.bat ___DYNAMODB_TABLE_STRING___ DEV-ASP.NET_SessionState c:\temp\Web1.text > c:\temp\Web2.text
-	call BatchSubstitute.bat ___SQS_QUEUE_NAME_STRING___ https://sqs.us-west-2.amazonaws.com/591516837963/DEV-dmw2-shapeorders-queue c:\temp\Web2.text > c:\temp\Web3.text
-	call BatchSubstitute.bat ___S3_BUCKET_NAME_STRING___ dev-dmw2-shapestoreresources c:\temp\Web3.text > c:\temp\Web4.text
-	call BatchSubstitute.bat ___MYSQLDB_DBNAME_STRING___ dmw2shapestoredb c:\temp\Web4.text > c:\temp\Web5.text
-	call BatchSubstitute.bat ___MYSQLDB_USER_STRING___ admin c:\temp\Web5.text > c:\temp\Web6.text
-	call BatchSubstitute.bat ___MYSQLDB_PASSWORD_STRING___ j57Q25oTosk2sWybLr3f; c:\temp\Web6.text > c:\temp\Web7.text
-	mv c:\temp\web7.text c:\inetpub\wwwroot\shapestore\Web.config
-#	del c:\temp\web?.text
+#
+#
+#
 }
