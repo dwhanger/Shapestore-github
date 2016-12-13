cfc3f9737f3f9e4207dbc754fc73b0a2462b0168
dwhanger <dwhanger@hotmail.com>
2016-12-13 11:25:26 -0800

    removed call to ps1 script and adjusted the if stmt

--- _scripts/after_install.bat
+++ _scripts/after_install.bat
@@ -1,12 +1,14 @@
 REM @ECHO OFF
-PowerShell.exe -ExecutionPolicy Bypass -Command "& '%~dpn0.ps1'"
+REM PowerShell.exe -ExecutionPolicy Bypass -Command "& '%~dpn0.ps1'"
 
 REM Fixup the strings in the web.config and other places after the deployment....
 REM 
 REM 
 
-If ( $DEPLOYMENT_GROUP_NAME == "TEST*" )
+If ( $DEPLOYMENT_GROUP_NAME == "Test-ShapestoreGroup" )
 {
+	echo "TEST..."
+
 	call BatchSubstitute.bat ___MYSQLDB_STRING___ testdmw2shapestoreinstance.cxkjrsk8xjz1.us-west-2.rds.amazonaws.com c:\inetpub\wwwroot\shapestore\Web.config > c:\temp\Web1.text
 	call BatchSubstitute.bat ___DYNAMODB_TABLE_STRING___ TEST-ASP.NET_SessionState c:\temp\Web1.text > c:\temp\Web2.text
 	call BatchSubstitute.bat ___SQS_QUEUE_NAME_STRING___ https://sqs.us-west-2.amazonaws.com/591516837963/TEST-dmw2-shapeorders-queue c:\temp\Web2.text > c:\temp\Web3.text
@@ -17,8 +19,10 @@ If ( $DEPLOYMENT_GROUP_NAME == "TEST*" )
 	mv c:\temp\web7.text c:\inetpub\wwwroot\shapestore\Web.config
 REM del c:\temp\web?.text
 }
-ElseIf ( $DEPLOYMENT_GROUP_NAME == "DEV*" )
+ElseIf ( $DEPLOYMENT_GROUP_NAME == "Dev-ShapestoreGroup" )
 {
+	echo "DEV..."
+
 	call BatchSubstitute.bat ___MYSQLDB_STRING___ dmw2shapestoreinstance.cxkjrsk8xjz1.us-west-2.rds.amazonaws.com c:\inetpub\wwwroot\shapestore\Web.config > c:\temp\Web1.text
 	call BatchSubstitute.bat ___DYNAMODB_TABLE_STRING___ DEV-ASP.NET_SessionState c:\temp\Web1.text > c:\temp\Web2.text
 	call BatchSubstitute.bat ___SQS_QUEUE_NAME_STRING___ https://sqs.us-west-2.amazonaws.com/591516837963/DEV-dmw2-shapeorders-queue c:\temp\Web2.text > c:\temp\Web3.text
