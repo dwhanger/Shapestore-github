80ec5fc921e4f994075b3daf249587cf525216f8
dwhanger <dwhanger@hotmail.com>
2016-12-20 10:13:57 -0800

    minor adjustments

--- _scripts/start_server.ps1
+++ _scripts/start_server.ps1
@@ -6,11 +6,10 @@ Start-Service -Name "w3svc"
 #
 # Create a new virtual directory...
 #
-New-WebVirtualDirectory -Site "Default Web Site" -Name shapestore -PhysicalPath c:\inetpub\wwwroot\shapestore -Force
+New-WebVirtualDirectory -Site "Default Web Site" -Name "shapestore" -PhysicalPath "$Env:systemdrive\inetpub\wwwroot\shapestore"
 
 #
 # Convert the virtual directory to a web application...
 #
-ConvertTo-WebApplication -ApplicationPool "DefaultAppPool" -Force "IIS:\Sites\Default Web Site\shapestore"
+ConvertTo-WebApplication -PSPath "IIS:\Sites\Default Web Site\shapestore"
 
-Get-ChildItem 'IIS:\Sites\Default Web Site'
